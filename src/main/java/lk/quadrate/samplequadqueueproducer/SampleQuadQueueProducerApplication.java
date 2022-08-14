cc94b2158b27d9a385ed37d1d2d1440773351db9
razmy <mohamedrazmy@efuturesworld.com>
2022-08-14 14:25:21 +0530

    initial commit

--- src/main/java/lk/quadrate/samplequadqueueproducer/SampleQuadQueueProducerApplication.java
+++ src/main/java/lk/quadrate/samplequadqueueproducer/SampleQuadQueueProducerApplication.java
@@ -1,11 +1,12 @@
 package lk.quadrate.samplequadqueueproducer;
 
+import lk.quadrate.quadqueueclient.annotation.EnableQuadClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "lk.quadrate")
-@EnableFeignClients(basePackages = {"lk.quadrate.quadqueueclient"})
+@EnableQuadClient
 public class SampleQuadQueueProducerApplication {
 
     public static void main(String[] args) {
